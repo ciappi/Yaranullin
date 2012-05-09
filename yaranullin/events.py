476abfec56838d633b38dd6388434d82b2c30f82
Marco Scopesi <marco.scopesi@gmail.com>
2012-05-09 18:41:58 +0200

    yaranullin/events.py: handle weakref
    All this is needed because it is not possible to save directly a bound method
    as a weakref.

--- yaranullin/events.py
+++ yaranullin/events.py
@@ -3,22 +3,26 @@ import inspect
 import time
 import weakref
 
+
 _EVENTS = {}
 
 _QUEUE = collections.deque()
 
 ANY, QUIT, TICK = range(3)
 
-_EVENTS[ANY] = weakref.WeakSet()
+_EVENTS[ANY] = weakref.WeakValueDictionary()
 
 
 def register(event, func):
-    if not callable(func):
-        # XXX could raise an exception
+    if inspect.isfunction(func):
+        func = {func: func}
+    elif inspect.ismethod(func):
+        func = {func.im_func: func.im_self}
+    else:
         return
     if event not in _EVENTS:
-        _EVENTS[event] = weakref.WeakSet()
-    _EVENTS[event].add(func)
+        _EVENTS[event] = weakref.WeakValueDictionary()
+    _EVENTS[event].update(func)
 
 
 def unregister(event, func=None):
@@ -28,9 +32,11 @@ def unregister(event, func=None):
     if func is None and event is not ANY:
         del _EVENTS[event]
         return
-    if func in _EVENTS[event]:
-        _EVENTS[event].remove(func)
-    if not len(_EVENTS[event]) and event is not ANY:
+    if inspect.ismethod(func):
+        del _EVENTS[event][func.im_func]
+    else:
+        del _EVENTS[event][func]
+    if not _EVENTS[event] and event is not ANY:
         del _EVENTS[event]
 
 
@@ -44,12 +50,15 @@ def _consume_event_queue():
     stop = False
     while _QUEUE:
         event, ekargs = _QUEUE.popleft()
-        handlers = _EVENTS[event] | _EVENTS[ANY]
-        for handler in handlers:
+        handlers = {}
+        handlers.update(_EVENTS[event])
+        handlers.update(_EVENTS[ANY])
+        for handler, instance in handlers.iteritems():
             hargs, _, hkeywords, _ = inspect.getargspec(handler)
             kargs = dict(ekargs)
             # Add a special attribute with the type of the event
             kargs['__event__'] = event
+            kargs['self'] = instance
             if not hkeywords:
                 for key in kargs:
                     if key not in hargs:
