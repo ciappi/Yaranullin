4c318e3373a81bb63222e98cf413336b895e4d6a
Marco Scopesi <marco.scopesi@gmail.com>
2012-05-10 08:20:17 +0200

    yaranullin/events.py: add some docstrings and comments

--- yaranullin/events.py
+++ yaranullin/events.py
@@ -1,19 +1,42 @@
+# yaranullin/events.py
+#
+# Copyright (c) 2012 Marco Scopesi <marco.scopesi@gmail.com>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+'''The event system of Yaranullin.
+
+This module is an simple implementation of an event patter.
+
+'''
+
 import collections
 import inspect
 import time
 import weakref
 
 
-_EVENTS = {}
+ANY, QUIT, TICK = range(3)
 
 _QUEUE = collections.deque()
 
-ANY, QUIT, TICK = range(3)
+_EVENTS = {}
 
 _EVENTS[ANY] = weakref.WeakValueDictionary()
 
 
 def register(event, func):
+    ''' Register an handler '''    
     if inspect.isfunction(func):
         func = {func: func}
     elif inspect.ismethod(func):
@@ -26,10 +49,12 @@ def register(event, func):
 
 
 def unregister(event, func=None):
+    ''' Unregister an handler '''
     if event not in _EVENTS:
         # XXX may raise an exception
         return
     if func is None and event is not ANY:
+        # Delete all the events
         del _EVENTS[event]
         return
     if inspect.ismethod(func):
@@ -41,15 +66,19 @@ def unregister(event, func=None):
 
 
 def post(event, **kargs):
+    ''' Post an event '''
     if event not in _EVENTS and not _EVENTS[ANY]:
         return
+    # Post an event only if there is some handler registered.
     _QUEUE.append((event, kargs)) 
 
 
 def _consume_event_queue():
+    ''' Consume the event queue and call all handlers '''
     stop = False
     while _QUEUE:
         event, ekargs = _QUEUE.popleft()
+        # Find all handler for this event
         handlers = {}
         handlers.update(_EVENTS[event])
         handlers.update(_EVENTS[ANY])
@@ -59,8 +88,11 @@ def _consume_event_queue():
             # Add a special attribute with the type of the event
             kargs['__event__'] = event
             if 'self' in hargs:
+                # We assume that this is a bound method
                 kargs['self'] = self
+            # Check if the handler has ** magic
             if not hkeywords:
+                # Delete all arguments that the handler cannot take
                 for key in kargs:
                     if key not in hargs:
                         del kargs[key]
@@ -99,11 +131,13 @@ class Pipe(object):
         register(TICK, self.tick)
 
     def handle(self, **kargs):
+        ''' Put given event to the out queue '''
         if kargs['__event__'] == TICK:
             return
         self.out_queue.put(kargs)
 
     def tick(self):
+        ''' Get all the event from the in queue '''
         while not self.in_queue.empty():
             event = self.in_queue.get()
             post(**event)
