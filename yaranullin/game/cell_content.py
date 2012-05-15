d9d139a57b15848d26780f5925d7eb782fc88e57
Marco Scopesi <marco.scopesi@gmail.com>
2012-05-15 15:38:03 +0200

    yaranullin/game/cell_content.py: rewrite

--- yaranullin/game/cell_content.py
+++ yaranullin/game/cell_content.py
@@ -14,57 +14,21 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-import logging
 
-from yaranullin.event_system import Listener
+class _CellContent(object):
+
+    ''' Generic content of a board's cell '''
+
+    def __init__(self, width, height):
+        self.pos = None
+        self.size = width, height
 
 
-class CellContentInitializationError(Exception):
-    """Thrown an exception if there is a placement error inside __init__."""
+class Pawn(_CellContent):
 
+    '''A PG, PNG or a monster '''
 
-class CellContent(Listener):
-
-    """The content of a cell.
-    
-    It is subclass of Listener but actually use any of the parent methods.
-    
-    """
-
-    def __init__(self, board, x, y, width, height, rotated):
-        Listener.__init__(self, board)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-        self.rotated = False
-        placed = self.place(x, y, rotated)
-        if not placed:
-            logging.info('Unable to create CellContent at (' + str(self.x) +
-                         ',' + str(self.y) + ')')
-            raise CellContentInitializationError
-
-    @property
-    def grid(self):
-        """A read only reference to the grid."""
-        return self.event_manager.grid
-
-    def place(self, x, y, rotate):
-        """Place the CellContent on the grid."""
-        if rotate:
-            width, height = self.height, self.width
-        else:
-            width, height = self.width, self.height
-        placed = self.grid.set_content(x, y, width, height, self)
-        if not placed:
-            logging.info('Cannot move ' + str(self) + ' to (' + str(x) +
-                         ',' + str(y) + ')')
-        # Boolean xor.
-        self.rotated = self.rotated != rotate
-        return placed
-
-    def move(self, dx, dy, rotate):
-        """Move the CellContent on the grid."""
-        x, y = self.x + dx, self.y + dy
-        moved = self.place(x, y, rotate)
-        return moved
+    def __init__(self, name, initiative, width, height):
+        _CellContent.__init__(self, width, height)
+        self.name = name
+        self.initiative = initiative
