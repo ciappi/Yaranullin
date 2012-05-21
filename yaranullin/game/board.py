2bebf6e4340af93644fc307e22f8637534eaa3ea
Marco Scopesi <marco.scopesi@gmail.com>
2012-05-21 22:46:14 +0200

    yaranullin/game/board.py: use new grid API

--- yaranullin/game/board.py
+++ yaranullin/game/board.py
@@ -14,8 +14,14 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+''' Board object '''
+
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
 from yaranullin.game.cell_content import Pawn
-from yaranullin.game.grid import Grid, CellsAreNotEmpty, NotAValidPosition
+from yaranullin.game.grid import Grid
 
 
 class Board(object):
@@ -23,41 +29,71 @@ class Board(object):
     ''' The board where the pawns lie '''
 
     def __init__(self, name, size):
+        LOGGER.debug("Initializing board '%s' with size (%d, %d)...", name,
+                size[0], size[1])
         self.name = name
         self.size = size
         self.initiatives = []
         self.pawns = {}
-        self.grid = Grid(width, height)
+        self._grid = Grid(size)
+        LOGGER.debug("Initializing board '%s' with size (%d, %d)... done", name,
+                size[0], size[1])
+
+    def _place_pawn(self, pawn, pos, size):
+        ''' Place a pawn on the grid '''
+        contents = self._grid.get(pos, size)
+        # Add a pawn only if the cells are empty or taken by this pawn
+        if contents and pawn not in contents:
+            raise IndexError
+        self._grid.remove(pawn)
+        self._grid.add(pawn, pos, size)
 
     def create_pawn(self, name, initiative, pos, size):
         ''' Create a new Pawn '''
+        LOGGER.debug("Creating a new pawn...")
         pawn = Pawn(name, initiative, size)
         try:
-            self.grid.place(pawn, pos)
-        except (NotAValidPosition, CellsAreNotEmpty):
-            pass
+            self._place_pawn(pawn, pos, size)
+        except IndexError:
+            LOGGER.warning("Cannot create pawn '%s' at pos (%d, %d) with "
+                "size (%d, %d)", pawn.name, pos[0], pos[1], size[0], size[1])
         else:
             self.pawns[pawn.name] = pawn
             self.initiatives.append(pawn)
             self.initiatives.sort(key=lambda pawn: pawn.initiative,
                     reverse=True)
+            LOGGER.debug("Creating a new pawn... done")
             return pawn
 
     def del_pawn(self, name):
         ''' Delete the pawn 'name' '''
+        LOGGER.debug("Removing pawn '%s'...", name)
         try:
             pawn = self.pawns.pop(name)
         except KeyError:
-            pass
+            LOGGER.warning("Pawn '%s' was not in board '%s'", pawn.name,
+                self.name)
         else:
             self.initiatives.remove(pawn)
+            self._grid.remove(pawn)
+            LOGGER.debug("Removing pawn '%s'... done", name)
             return pawn
 
-    def move_pawn(self, pawn, pos):
+    def move_pawn(self, name, pos, size=None):
         ''' Move the pawn 'name' to pos'''
+        LOGGER.debug("Moving pawn '%s' to (%d, %d)...", name, pos[0], pos[1])
         try:
-            self.grid.place(pawn, pos)
-        except (NotAValidPosition, CellsAreNotEmpty):
-            pass
+            pawn = self.pawns[name]
+            if size is None:
+                size = pawn.size
+            self._place_pawn(pawn, pos, size)
+        except KeyError:
+            LOGGER.warning("Pawn '%s' was not in board '%s'", name,
+                self.name)
+        except IndexError:
+            LOGGER.warning("Cannot move pawn '%s' at pos (%d, %d) with "
+                "size (%d, %d)", pawn.name, pos[0], pos[1], size[0], size[1])
         else:
+            LOGGER.debug("Moving pawn '%s' to (%d, %d)... done", name, pos[0],
+                    pos[1])
             return True
