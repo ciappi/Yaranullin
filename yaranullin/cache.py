43a1c257917e4304832ffdef31ce86393add990a
Marco Scopesi <marco.scopesi@gmail.com>
2012-12-04 23:10:07 +0100

    yaranullin:cache.py: handle multiple arguments in loader function

--- yaranullin/cache.py
+++ yaranullin/cache.py
@@ -16,8 +16,8 @@
 
 import os
 
-from yaranullin.event_system import connect, post
 from yaranullin.config import YR_CACHE_DIR
+from yaranullin.event_system import connect, post
 
 
 #
@@ -26,26 +26,28 @@ from yaranullin.config import YR_CACHE_DIR
 _CACHE = {}
 
 
-def cache(func):
+def cache(loader):
     ''' Cache decorator '''
 
-    def _cache(resource_name):
+    def _cache(resource_name, *args, **kargs):
         if not isinstance(resource_name, basestring):
             raise RuntimeError('cache._cache(): invalid name for a cached '
                     'object')
         try:
-            return _CACHE[hash(func), resource_name]
+            return _CACHE[hash(loader), resource_name, args,
+                tuple(kargs.items())]
         except KeyError:
             try:
-                cached_obj = func(os.path.join(YR_CACHE_DIR, resource_name))
+                cached_obj = loader(resource_name, *args, **kargs)
             except IOError as why:
                 if why.errno == 2:
-                    # The file is not available, ask the server...
+                    # There is a missing file, ask the server...
                     post('resource-request', name=resource_name)
                 else:
                     raise
             else:
-                _CACHE[hash(func), resource_name] = cached_obj
+                _CACHE[hash(loader), resource_name, args,
+                    tuple(kargs.items())] = cached_obj
                 return cached_obj
 
     return _cache
@@ -59,7 +61,7 @@ def update_cache(event_dict):
         file_.write(resource)
 
 #
-# As soon as this module is imported, function 'update_cache' is connected to
+# As soon as this module is imported, 'update_cache' is connected to
 # the 'resource-update' event.
 #
 connect('resource-update', update_cache)
