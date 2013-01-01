6ab5a66d8d7a55200f35407e6ea7b0632ff48386
Marco Scopesi <marco.scopesi@gmail.com>
2013-01-01 22:29:52 +0100

    yaranullin/run_client.py: purge pygame

--- yaranullin/run_client.py
+++ yaranullin/run_client.py
@@ -15,16 +15,6 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 import asyncore
-try:
-    import pygame
-    import pygame.locals as PL
-except ImportError:
-    PYGAME = False
-else:
-    from yaranullin.pygame_.gui import PygameGui
-    from yaranullin.pygame_.controllers import PygameKeyboard,\
-        PygameMouse, PygameSystem
-    PYGAME = True
 
 from yaranullin.config import CONFIG
 from yaranullin.event_system import post, process_queue
@@ -37,32 +27,13 @@ ClientEndPoint()
 HOST = CONFIG.get('network', 'host')
 PORT = CONFIG.getint('network', 'port')
 GAME = DummyGameWrapper()
-if PYGAME:
-    pygame.init()
-    pygame.event.set_allowed(None)
-    pygame.event.set_allowed([PL.QUIT, PL.MOUSEMOTION, PL.MOUSEBUTTONUP,
-                              PL.MOUSEBUTTONDOWN, PL.KEYDOWN, PL.KEYUP])
-    pygame.display.set_mode((0, 0))
-    KEYBOARD = PygameKeyboard()
-    MOUSE = PygameMouse()
-    SYSTEM = PygameSystem()
-    GUI = PygameGui()
 
 
 def run(args):
     ''' Main loop for the client '''
     post('join', host=HOST, port=PORT)
     stop = False
-    if PYGAME:
-        clock = pygame.time.Clock()
     while not stop:
-        if PYGAME:
-            dt = clock.tick(60) / 1000.0
-            post('tick', dt=dt)
-            pygame.display.flip()
-        else:
-            post('tick')
+        post('tick')
         stop = process_queue()
         asyncore.poll(0.002)
-    if PYGAME:
-        pygame.quit()
